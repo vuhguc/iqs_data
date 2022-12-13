f8e5e8af06fcc96a15e1619c8c1fc296ac9d42cb
Hoang Vu <vuhg@mail.uc.edu>
2022-12-12 21:23:30 -0500

    add README.md

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# iqs_data
+# iqs\_data
+
+## Dependencies
+* Python 3
+* MySQL
+* Elasticsearch
+* Logstash
+* JDBC
+
+## Run
+Run the following commands in order:
+1. `python3 01_download_csv_files.py`
+2. `python3 02_create_mysql_table.py`
+3. `python3 03_create_elasticsearch_index.py`
+
+## Config
+Edit variables in `config.py`. Important variables are:
+1. `NIH_USERNAME` and `NIH_PASSWORD`: see the below section
+2. `MYSQL_TABLENAME`: set this to the name of the MySQL table storing the article data
+3. `MYSQL_CHARSET` and `MYSQL_COLLATE`: change these if you get encoding errors or charset/collate not found, different versions of MySQL may have different charset/collate
+4. `ELASTICSEARCH_INDEXNAME`: set this to the name of the Elasticsearch index storing the article data
+5. `JDBC_DRIVER_LIBRARY`: set this to the correct path of the jdbc jar file on the system
+6. `LOGSTASH_BINNAME`: set this to the correct path of the logstash run file on the system
+7. `PUBDATE_RANGES`: include publication date ranges you want to download; don't make the range too large (> 1 month) or the server may fail to create a csv file
+
+## Find `NIH_USERNAME` and `NIH_PASSWORD`
+The default values for these two variables work upto December 2022. In case NIH changes the anonymous username and password, use the following steps to find them:
+1. In your browser, open the developer tools (`ctrl + shift + I` for Google Chromes).
+2. Enter the Network tab.
+3. Enter this url: `https://icite.od.nih.gov/covid19/search/`.
+4. Check the login request. You can see the values for `NIH_USERNAME` and `NIH_PASSWORD` in the Payload tab.
+![nih\_authentication.png](img/nih_authentication.png)
--- config.py
+++ config.py
@@ -8,29 +8,29 @@ NIH_DOWNLOAD_WAIT_TIME = 10
 
 NIH_DOWNLOAD_NUM_RETRIES = 10
 
-MYSQL_USERNAME = 'hoang'
-# MYSQL_USERNAME = 'pubmed'
+# MYSQL_USERNAME = 'hoang'
+MYSQL_USERNAME = 'pubmed'
 
-MYSQL_PASSWORD = '123456'
-# MYSQL_PASSWORD = 'Dummy@123'
+# MYSQL_PASSWORD = '123456'
+MYSQL_PASSWORD = 'Dummy@123'
 
 MYSQL_TABLENAME = 'cord_19.nih_20220907'
 
 MYSQL_CHARSET = 'utf8mb4'
 
-MYSQL_COLLATE = 'utf8mb4_unicode_520_ci'
-# MYSQL_COLLATE = 'utf8mb4_0900_ai_ci'
+# MYSQL_COLLATE = 'utf8mb4_unicode_520_ci'
+MYSQL_COLLATE = 'utf8mb4_0900_ai_ci'
 
 ELASTICSEARCH_INDEXNAME = 'nih_20220907'
 
-JDBC_DRIVER_LIBRARY = '/usr/share/java/mariadb-jdbc/mariadb-java-client.jar'
-# JDBC_DRIVER_LIBRARY = '/usr/share/java/mysql-connector-java-8.0.11.jar'
+# JDBC_DRIVER_LIBRARY = '/usr/share/java/mariadb-jdbc/mariadb-java-client.jar'
+JDBC_DRIVER_LIBRARY = '/usr/share/java/mysql-connector-java-8.0.11.jar'
 
-JDBC_DRIVER_CLASS = 'org.mariadb.jdbc.Driver'
-# JDBC_DRIVER_CLASS = 'com.mysql.jdbc.Driver'
+# JDBC_DRIVER_CLASS = 'org.mariadb.jdbc.Driver'
+JDBC_DRIVER_CLASS = 'com.mysql.jdbc.Driver'
 
-JDBC_CONNECTION_STRING = 'jdbc:mariadb://localhost:3306?useCursorFetch=true'
-# JDBC_CONNECTION_STRING = 'jdbc:mysql://localhost:3306?useCursorFetch=true'
+# JDBC_CONNECTION_STRING = 'jdbc:mariadb://localhost:3306?useCursorFetch=true'
+JDBC_CONNECTION_STRING = 'jdbc:mysql://localhost:3306?useCursorFetch=true'
 
 LOGSTASH_BINNAME = '/usr/share/logstash/bin/logstash'
 
@@ -48,28 +48,28 @@ PUBDATE_RANGES = [
     ('2020-10-01', '2020-10-31'),
     ('2020-11-01', '2020-11-30'),
     ('2020-12-01', '2020-12-31'),
-#     ('2021-01-01', '2021-01-31'),
-#     ('2021-02-01', '2021-02-28'),
-#     ('2021-03-01', '2021-03-31'),
-#     ('2021-04-01', '2021-04-30'),
-#     ('2021-05-01', '2021-05-31'),
-#     ('2021-06-01', '2021-06-30'),
-#     ('2021-07-01', '2021-07-31'),
-#     ('2021-08-01', '2021-08-31'),
-#     ('2021-09-01', '2021-09-30'),
-#     ('2021-10-01', '2021-10-31'),
-#     ('2021-11-01', '2021-11-30'),
-#     ('2021-12-01', '2021-12-31'),
-#     ('2022-01-01', '2022-01-31'),
-#     ('2022-02-01', '2022-02-28'),
-#     ('2022-03-01', '2022-03-31'),
-#     ('2022-04-01', '2022-04-30'),
-#     ('2022-05-01', '2022-05-31'),
-#     ('2022-06-01', '2022-06-30'),
-#     ('2022-07-01', '2022-07-31'),
-#     ('2022-08-01', '2022-08-31'),
-#     ('2022-09-01', '2022-09-30'),
-#     ('2022-10-01', '2022-10-31'),
-#     ('2022-11-01', '2022-11-30'),
-#     ('2022-12-01', '2022-12-31')
+    ('2021-01-01', '2021-01-31'),
+    ('2021-02-01', '2021-02-28'),
+    ('2021-03-01', '2021-03-31'),
+    ('2021-04-01', '2021-04-30'),
+    ('2021-05-01', '2021-05-31'),
+    ('2021-06-01', '2021-06-30'),
+    ('2021-07-01', '2021-07-31'),
+    ('2021-08-01', '2021-08-31'),
+    ('2021-09-01', '2021-09-30'),
+    ('2021-10-01', '2021-10-31'),
+    ('2021-11-01', '2021-11-30'),
+    ('2021-12-01', '2021-12-31'),
+    ('2022-01-01', '2022-01-31'),
+    ('2022-02-01', '2022-02-28'),
+    ('2022-03-01', '2022-03-31'),
+    ('2022-04-01', '2022-04-30'),
+    ('2022-05-01', '2022-05-31'),
+    ('2022-06-01', '2022-06-30'),
+    ('2022-07-01', '2022-07-31'),
+    ('2022-08-01', '2022-08-31'),
+    ('2022-09-01', '2022-09-30'),
+    ('2022-10-01', '2022-10-31'),
+    ('2022-11-01', '2022-11-30'),
+    ('2022-12-01', '2022-12-31')
 ]
